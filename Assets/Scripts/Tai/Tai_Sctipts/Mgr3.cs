e273f08a68ef7a4d4961a4b0836dba6dcdbb41e7
Taiyun <wendy0712000@gmail.com>
2024-08-25 04:59:56 +0800

    轉場ed

--- Assets/Scripts/Tai/Tai_Sctipts/Mgr3.cs
+++ Assets/Scripts/Tai/Tai_Sctipts/Mgr3.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Mgr3 : MonoBehaviour
 {
     public GameObject teachPanel;
+
+    public Animator fade;
     void Start()
     {
         
@@ -17,5 +20,17 @@ public class Mgr3 : MonoBehaviour
         {
             teachPanel.SetActive(false);
         }
+        
+        //ED測試鍵
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            fade.SetTrigger("fade");
+            Invoke("LoadED",2f);
+        }
+    }
+
+    void LoadED()
+    {
+        SceneManager.LoadScene("Tai_sED");
     }
 }

544a606226753d9d1d20a62f1cad882f844ef98f
GreenLeaf0w0 <wujoe0415@gmail.com>
2024-08-25 14:30:37 +0800

    add cg

--- Assets/Scripts/GreenLeaf/Skill System/Defend.cs
+++ Assets/Scripts/GreenLeaf/Skill System/Defend.cs
@@ -6,6 +6,13 @@ public class Defend : BasicSkill
 {
     public float Defense = 0.5f;
     private float _pastDefense = 0f;
+    public GameObject Sheild;
+    private GameObject _sheild;
+    private void Start()
+    {
+        _sheild = Instantiate(Sheild, transform);
+        _sheild.SetActive(false);
+    }
     public override bool CanUseSkill()
     {
         if(CoolDownTime > CurrentCoolDownTime)
@@ -18,12 +25,13 @@ public class Defend : BasicSkill
     {
         _pastDefense = PlayerStatus.Instance.DefensePoint;
         PlayerStatus.Instance.DefensePoint = Defense;
+        _sheild.SetActive(true);
         Invoke("ResetDefense", Mathf.Min(0.4f, CoolDownTime - 0.1f));
     }
     private void ResetDefense()
     {
         PlayerStatus.Instance.DefensePoint = _pastDefense;
-        OnSkillRelease.Invoke();
+        _sheild.SetActive(false);
     }
     public override void LevelUp()
     {
